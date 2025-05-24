9dd1f24298522830a8e66a03e47d14bf9737596d
Space Wolf <wolfmanIII@gmail.com>
2025-05-24 18:21:23 +0200

    git ignored

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,6 @@ from sqlalchemy import pool
 from alembic import context
 
 from model import Base
-from model import ship, crew, crew_role, annual_budget, ship_mortgage
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
