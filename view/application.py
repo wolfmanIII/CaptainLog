3a31c7927ff28e70f1c1b91a9966be24af201e1c
SpaceWolf <wolfmanIII@gmail.com>
2025-05-26 20:12:16 +0200

    Menu init

--- view/application.py
+++ view/application.py
@@ -11,8 +11,7 @@ from view.ship_role_list import ShipRoleList
 
 class Application(ttk.Frame):
     def __init__(self, master):
-        ttk.Frame.__init__(master, width=700, height=500)
-        #super().__init__(master, width=700, height=500)
+        super().__init__(master, width=700, height=500)
         Menubar(master)
         self.grid(column=0, row=0, rowspan=2, columnspan=2)
         self.grid_propagate(False)
