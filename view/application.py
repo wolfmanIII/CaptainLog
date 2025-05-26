73797736108577c70fd95a0e25f15d43c412ebd7
SpaceWolf <wolfmanIII@gmail.com>
2025-05-27 00:44:13 +0200

    menubar

--- view/application.py
+++ view/application.py
@@ -1,7 +1,7 @@
 from os import name
 from sqlalchemy import column, select
 import tkinter
-from tkinter import ttk
+from tkinter import Tk, ttk
 
 from model.ship_role import ShipRole
 from service.dblink import DBLink
@@ -10,7 +10,7 @@ from view.ship_role_list import ShipRoleList
 
 
 class Application(ttk.Frame):
-    def __init__(self, master):
+    def __init__(self, master: Tk):
         super().__init__(master, width=700, height=500)
         Menubar(master)
         self.grid(column=0, row=0, rowspan=2, columnspan=2)
