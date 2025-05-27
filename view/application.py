8f5c89cb7f93fe1e8f5b9d38c2323109bedf95cf
antonio.pascucci <antonio.pascucci@performer.it>
2025-05-27 18:08:06 +0200

    Tk

--- view/application.py
+++ view/application.py
@@ -8,7 +8,7 @@ from service.dblink import DBLink
 from view.menubar import Menubar
 
 class Application(ttk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master: Tk):
         super().__init__(master)
         #self.grid(column=0, row=0, rowspan=2, columnspan=2)
         self.pack(fill="both", expand=True)
