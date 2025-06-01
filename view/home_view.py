db5dc7b2dbdc35f19215bd5909f546440c783f6e
SpaceWolf <wolfmanIII@gmail.com>
2025-06-01 13:24:07 +0200

    home create_widtgets function

--- view/home_view.py
+++ view/home_view.py
@@ -6,19 +6,22 @@ from numpy import size
 class HomeView(ttk.Frame):
     def __init__(self, master, router):
         super().__init__(master)
+        self.router = router
+        self.create_widgets(master)
 
+    def create_widgets(self, master):
         master.columnconfigure(0, weight=1)
         ttk.Label(self, text="Captain Log", font=("", 18)).grid(column=0, row=0, padx=10, pady=10, sticky="w")
 
-        buttonGroup = ButtonGroup(self, router)
+        buttonGroup = ButtonGroup(self)
         buttonGroup.grid(column=0, row=1, padx=5, pady=5, sticky="w")
 
 class ButtonGroup(ttk.Frame):
-    def __init__(self, parent, router):
+    def __init__(self, parent):
         super().__init__(parent, borderwidth=1, relief="solid")
 
-        ttk.Button(self, text="Ships", command=lambda: router.navigate("ships")).grid(column=0, row=1, padx=10, pady=10)
-        ttk.Button(self, text="Crew", command=lambda: router.navigate("crew")).grid(column=1, row=1, padx=10, pady=10)
+        ttk.Button(self, text="Ships", command=lambda: parent.router.navigate("ships")).grid(column=0, row=1, padx=10, pady=10)
+        ttk.Button(self, text="Crew", command=lambda: parent.router.navigate("crew")).grid(column=1, row=1, padx=10, pady=10)
         ttk.Button(self, text="Ship Mortgage").grid(column=2, row=1, padx=10, pady=10)
         ttk.Button(self, text="Annual Budget").grid(column=3, row=1, padx=10, pady=10)
         ttk.Button(self, text="Contracts").grid(column=4, row=1, padx=10, pady=10)
